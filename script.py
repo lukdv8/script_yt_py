6235c69bc2976843e91fe1276560e6e9725145b8
Lucas Duarte <lukvig@hotmail.com>
2024-10-25 21:14:39 -0300

    init(script): baixa o audio de um video do youtube

---
+++ script.py
@@ -0,0 +1,23 @@
+from pytubefix import YouTube
+import os
+
+def baixar_audio(url):
+    try:
+        yt = YouTube(url)
+    
+        audio_stream = yt.streams.filter(only_audio=True).first()
+
+        output = audio_stream.download()
+
+        base, ext = output.rsplit('.', 1)
+        novo_arquivo = base + '.mp3'
+        os.rename(output, novo_arquivo)
+
+        print(f"Áudio baixado com sucesso: {novo_arquivo}")
+
+    except Exception as e:
+        print(e)
+
+url = "https://www.youtube.com/watch?v=nMzPHAAnyHc&ab_channel=DJLP7-SOLTAOBEATPRAELASEMBRAZACACHORRO"
+
+baixar_audio(url)
